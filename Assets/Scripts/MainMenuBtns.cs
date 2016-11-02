93446a1ce5c754aff19e5911630e9c76bfdd9049
torresmatt <torresmatt@gmail.com>
2016-11-02 15:26:09 -0500

    first commit

---
+++ Assets/Scripts/MainMenuBtns.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuBtns : MonoBehaviour 
+{
+
+    public string FirstScene;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(FirstScene);
+    }
+}
