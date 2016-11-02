268ddda6cb68b388fb044add570069c4c5207ad0
Sparrowhawk <sparrowhawk@Sparrowhawk-MBPro.local>
2016-11-02 15:52:50 -0500

    Added press any key functionality to splash screen

---
+++ Assets/Scripts/PressAnyKey.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PressAnyKey : MonoBehaviour {
+
+    public string nextScene;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+	}
+}
